a5ef39f358f7b45cdbca65c05ffff29cdb5f81a3
kphamgit <kevinpham.us@gmail.com>
2026-02-09 14:43:17 -0500

    version 02-09-2026 added ping message to websocket to keep the connection open

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const Redis = require("ioredis");
+require("dotenv").config(); // Load environment variables from .env file
 
 const WebSocket = require("ws");
 //const wss = new WebSocket.Server({ port: 8080 });
@@ -7,17 +8,40 @@ const wss = new WebSocket.Server({ port: PORT }); // either PORT from .env or
 // assigned automatically by hosting provider (e.g., Heroku) when deployed
 
 // Create Redis clients with TLS options
+/*
 const redisOptions = {
     tls: {
       rejectUnauthorized: false, // Allow self-signed certificates
     },
   };
+  */
+
+  console.log("Running in environment: ", process.env.NODE_ENV);
+
+  const redisOptions = process.env.NODE_ENV === "production" ? {
+    tls: {
+      rejectUnauthorized: false, // Allow self-signed certificates in production
+    },
+    connectTimeout: 20000, // Increase timeout to 20 seconds
+  } : {}; // No TLS options in development
+  
   
 
 const REDIS_URL = process.env.REDIS_URL || "redis://localhost:6379"; // Use REDIS_URL from environment variable or default to local Redis
 // Create two Redis clients: one for subscribing and one for publishing/other commands
+console.log("Connecting to Redis at: ", REDIS_URL);
+
 const subscriber = new Redis(REDIS_URL, redisOptions); // For subscribing to channels
 const publisher = new Redis(REDIS_URL, redisOptions);  // For publishing messages or other Redis commands
+
+subscriber.on("error", (err) => {
+    console.error("Redis subscriber error: ", err);
+    }
+);
+
+publisher.on("error", (err) => {
+    console.error("Redis publisher error: ", err);
+    });
 
 // Subscribe to the "notifications" channel
 subscriber.subscribe("notifications", (err, count) => {
@@ -61,6 +85,14 @@ wss.on("connection", (ws) => {
   console.log("Client connected via native WebSocket.");
   ws.send(JSON.stringify("Welcome to the WebSocket server!"));
 
+  ws.on("message", (msg) => {
+        console.log(`Received message from client: ${msg}`);
+    if (msg.type === "ping") {
+            console.log("Received 'ping' from client. Responding with 'pong'.");
+            // you don't have to respond to pings if you don't want to, but it's a common convention to do so
+        ws.send("pong");
+        }
+    });
 
   /*
   ws.on("message", (msg) => {
